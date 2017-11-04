8933c5c5e4e24782b8fb7c0cb91d68e83c9a550b
Andrea Baldacci, Ph.D <andrea@transformandlighting.com>
2017-11-04 19:06:07 +0100

    Update AddCeresCXX11RequirementsToTarget.cmake

--- cmake/AddCeresCXX11RequirementsToTarget.cmake
+++ cmake/AddCeresCXX11RequirementsToTarget.cmake
@@ -43,42 +43,8 @@ function(add_ceres_cxx11_requirements_to_target)
     endif()
 
     if (COMPILER_HAS_CXX11_FLAG)
-      # IMPORTANT: It is not sufficient to specify the
-      #            CXX_STANDARD/CXX_STANDARD_REQUIRED target properties
-      #            as these target properties are NOT exported.
-      if (CMAKE_VERSION VERSION_LESS "2.8.12")
-        # CMake version < 2.8.12 does not support target_compile_options(), warn
-        # user that they will have to add compile flags to their own projects
-        # manually.
-        message(WARNING "-- Warning: Detected CMake version: ${CMAKE_VERSION} "
-          "< 2.8.12, which is the minimum required for compile options to be "
-          "included in an exported CMake target and the detected. compiler "
-          "requires -std=c++11. The client is responsible for adding "
-          "-std=c++11 when linking against: ${TARGET}.")
-      elseif (COMMAND target_compile_features)
-        # CMake >= 3.1, use new target_compile_features() to specify Ceres'
-        # C++11 requirements as used in the public API.  This assumes that
-        # C++11 STL features are available if the specified features are
-        # available.  We do not use the cxx_std_11 feature to specify this as
-        # this did not come in until CMake 3.8.
-        #
-        # The reason to prefer using target_compile_features() if it exists is
-        # that this handles 'upgrading' of the C++ standard required more
-        # gracefully, e.g. if a client of Ceres requires C++14, but Ceres was
-        # compiled against C++11 then target_compile_options() may not work as
-        # expected.
-        target_compile_features(
-          ${TARGET} PUBLIC cxx_alignas cxx_alignof cxx_constexpr)
-      else()
-        # CMake version >= 2.8.12 && < 3.1 supports target_compile_options()
-        # but not target_compile_features(). For these intermediary versions,
-        # we use target_compile_options() to manually specify the C++11 flag and
-        # export it for client targets that depend on the target iff they are
-        # NOT compiling for C.  We check for not C, rather than C++ as
-        # LINKER_LANGUAGE is often NOTFOUND and then uses the default (C++).
         target_compile_options(${TARGET} PUBLIC
           $<$<NOT:$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,C>>:-std=c++11>)
-      endif()
     endif()
   endforeach()
 endfunction()
